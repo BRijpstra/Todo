e78c64cdf1fd5f3fb9d419d53ed0773d2503fe52
Berend Rijpstra <BRijpstra@gmail.com>
2020-11-12 11:32:44 +0100

    Calendar core build react

---
+++ task-cal/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+
+
+function App() {
+  return (
+    <div className="Calendar">
+     <Calendar />
+    </div>
+  );
+}
+
+export default App;
